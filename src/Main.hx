4b3ef501bc45cdd37dac818dd66379aab5cc436c
Anders Nissen <andnis@gmail.com>
2015-12-25 15:20:02 +0100

    Some notes

--- src/Main.hx
+++ src/Main.hx
@@ -13,11 +13,15 @@ class Main extends luxe.Game {
 
         // config.render.depth_bits = 24;
         // config.render.depth = true;
-
         return config;
     }
 
     override function ready() {
+        // Optional, set a consistent scale camera mode for the entire game
+		// this is a luxe's wip feature
+		// Luxe.camera.size = new luxe.Vector(800, 600);
+		// Luxe.camera.size_mode = luxe.Camera.SizeMode.fit;
+
         luxe.tween.Actuate.defaultEase = luxe.tween.easing.Quad.easeInOut;
 
         Luxe.renderer.clear_color.set(25/255, 35/255, 55/255);
@@ -27,6 +31,11 @@ class Main extends luxe.Game {
         states.add(new MinionActionsState());
         states.set(BattleState.StateId);
     }
+
+    // Scale camera's viewport accordingly when game is scaled, common and suitable for most games
+	// override function onwindowsized(e: luxe.Screen.WindowEvent) {
+    //     Luxe.camera.viewport = new luxe.Rectangle( 0, 0, e.event.x, e.event.y);
+    // }
 
     override function onkeyup(e :KeyEvent) {
         if (e.keycode == Key.enter && e.mod.alt) {
