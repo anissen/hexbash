d7fb59cdc201951adc3161dadd4b93e28a69e630
Anders Nissen <andnis@gmail.com>
2016-04-11 22:34:54 +0200

    Added postprocessing shader

--- src/Main.hx
+++ src/Main.hx
@@ -8,9 +8,61 @@ import filewatch.Filewatch;
 
 import game.states.*;
 
+
+// ------ PostProcess ------
+import phoenix.Batcher.BlendMode;
+import phoenix.RenderTexture;
+import phoenix.Texture;
+import phoenix.Batcher;
+import phoenix.Shader;
+import luxe.Sprite;
+import luxe.Vector;
+import luxe.Color;
+
+class PostProcess {
+    var output: RenderTexture;
+    var batch: Batcher;
+    var view: Sprite;
+    public var shader: Shader;
+
+    public function new(shader :Shader) {
+        output = new RenderTexture({ id: 'render-to-texture', width: Luxe.screen.w, height: Luxe.screen.h });
+        batch = Luxe.renderer.create_batcher({ no_add: true });
+        this.shader = shader;
+        view = new Sprite({
+            no_scene: true,
+            centered: false,
+            pos: new Vector(0,0),
+            size: Luxe.screen.size,
+            texture: output,
+            shader: shader, //Luxe.renderer.shaders.textured.shader,
+            batcher: batch
+        });
+    }
+
+    public function toggle() {
+        view.shader = (view.shader == shader ? Luxe.renderer.shaders.textured.shader : shader);
+    }
+
+    public function prerender() {
+        Luxe.renderer.target = output;
+        Luxe.renderer.clear(new Color(0,0,0,1));
+    }
+
+    public function postrender() {
+        Luxe.renderer.target = null;
+        Luxe.renderer.clear(new Color(1,0,0,1));
+        Luxe.renderer.blend_mode(BlendMode.src_alpha, BlendMode.zero);
+        batch.draw();
+        Luxe.renderer.blend_mode();
+    }
+}
+// ------ PostProcess ------
+
 class Main extends luxe.Game {
     static public var states :States;
     var fullscreen :Bool = false;
+    var postprocess :PostProcess;
 
     override function config(config :luxe.AppConfig) {
         config.render.antialiasing = 4;
@@ -23,6 +75,8 @@ class Main extends luxe.Game {
         config.preload.textures.push({ id: 'assets/images/icons/spider-alt.png' });
         config.preload.textures.push({ id: 'assets/images/icons/pointy-hat.png' });
         config.preload.texts.push({ id: 'assets/scripts/test.hxs' });
+        config.preload.shaders.push({ id: 'postprocess', frag_id: 'assets/shaders/postprocess.glsl', vert_id: 'default' });
+
 
         // config.render.depth_bits = 24;
         // config.render.depth = true;
@@ -39,6 +93,11 @@ class Main extends luxe.Game {
         luxe.tween.Actuate.defaultEase = luxe.tween.easing.Quad.easeInOut;
 
         Luxe.renderer.clear_color.set(25/255, 35/255, 55/255);
+
+        var shader = Luxe.resources.shader('postprocess');
+        shader.set_vector2('resolution', Luxe.screen.size);
+        postprocess = new PostProcess(shader);
+        postprocess.toggle();
 
         states = new States({ name: 'state_machine' });
         states.add(new BattleState());
@@ -91,10 +150,24 @@ class Main extends luxe.Game {
     //     });
     // }
 
+    override function onprerender() {
+        if (postprocess != null) postprocess.prerender();
+    }
+
+    override function update(dt :Float) {
+        if (postprocess != null) postprocess.shader.set_float('time', Luxe.core.tick_start + dt);
+    }
+
+    override function onpostrender() {
+        if (postprocess != null) postprocess.postrender();
+    }
+
     override function onkeyup(e :KeyEvent) {
         if (e.keycode == Key.enter && e.mod.alt) {
             fullscreen = !fullscreen;
             Luxe.snow.runtime.window_fullscreen(fullscreen, true /* true-fullscreen */);
+        } else if (e.keycode == Key.key_s) {
+            postprocess.toggle();
         } else if (e.keycode == Key.key_m) {
             states.set(WorldState.StateId);
         } else if (e.keycode == Key.key_b) {
