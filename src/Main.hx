535b587fd4a4d7500eb6336c53e4e83e4a64ce1f
Anders Nissen <andnis@gmail.com>
2016-04-24 20:40:15 +0200

    Refactor postprocess

--- src/Main.hx
+++ src/Main.hx
@@ -1,60 +1,9 @@
-
 import luxe.States;
 import luxe.Input.KeyEvent;
 import luxe.Input.Key;
 
+import game.PostProcess;
 import game.states.*;
-
-
-// ------ PostProcess ------
-import phoenix.Batcher.BlendMode;
-import phoenix.RenderTexture;
-import phoenix.Texture;
-import phoenix.Batcher;
-import phoenix.Shader;
-import luxe.Sprite;
-import luxe.Vector;
-import luxe.Color;
-
-class PostProcess {
-    var output: RenderTexture;
-    var batch: Batcher;
-    var view: Sprite;
-    public var shader: Shader;
-
-    public function new(shader :Shader) {
-        output = new RenderTexture({ id: 'render-to-texture', width: Luxe.screen.w, height: Luxe.screen.h });
-        batch = Luxe.renderer.create_batcher({ no_add: true });
-        this.shader = shader;
-        view = new Sprite({
-            no_scene: true,
-            centered: false,
-            pos: new Vector(0,0),
-            size: Luxe.screen.size,
-            texture: output,
-            shader: shader, //Luxe.renderer.shaders.textured.shader,
-            batcher: batch
-        });
-    }
-
-    public function toggle() {
-        view.shader = (view.shader == shader ? Luxe.renderer.shaders.textured.shader : shader);
-    }
-
-    public function prerender() {
-        Luxe.renderer.target = output;
-        Luxe.renderer.clear(new Color(0,0,0,1));
-    }
-
-    public function postrender() {
-        Luxe.renderer.target = null;
-        Luxe.renderer.clear(new Color(1,0,0,1));
-        Luxe.renderer.blend_mode(BlendMode.src_alpha, BlendMode.zero);
-        batch.draw();
-        Luxe.renderer.blend_mode();
-    }
-}
-// ------ PostProcess ------
 
 class Main extends luxe.Game {
     static public var states :States;
@@ -64,7 +13,6 @@ class Main extends luxe.Game {
     override function config(config :luxe.AppConfig) {
         config.render.antialiasing = 4;
 
-        // config.preload.textures.push({ id: 'assets/images/cultist.png' });
         config.preload.textures.push({ id: 'assets/images/tiles/tileGrass_full.png' });
         config.preload.textures.push({ id: 'assets/images/tiles/tileDirt_full.png' });
         config.preload.textures.push({ id: 'assets/images/tiles/tileSand_full.png' });
@@ -77,15 +25,7 @@ class Main extends luxe.Game {
         config.preload.textures.push({ id: 'assets/images/icons/shadow.png' });
         config.preload.textures.push({ id: 'assets/images/icons/background.png' });
         config.preload.texts.push({ id: 'assets/scripts/test.hxs' });
-        config.preload.shaders.push({ id: 'postprocess', frag_id: 'assets/shaders/postprocess.glsl', vert_id: 'default' });
-        // config.preload.shaders.push({ id: 'tilt_shift', frag_id: 'assets/shaders/tilt_shift.glsl', vert_id: 'default' });
-        // config.preload.shaders.push({ id: 'toon_water', frag_id: 'assets/shaders/toon_water.glsl', vert_id: 'default' });
-        // config.preload.shaders.push({ id: 'perlin_water', frag_id: 'assets/shaders/perlin_water.glsl', vert_id: 'default' });
-        // config.preload.shaders.push({ id: 'simple_water', frag_id: 'assets/shaders/simple_water.glsl', vert_id: 'default' });
-
-
-        // config.render.depth_bits = 24;
-        // config.render.depth = true;
+        config.preload.shaders.push({ id: 'postprocess', frag_id: 'assets/shaders/postprocess2.glsl', vert_id: 'default' });
         return config;
     }
 
@@ -114,16 +54,6 @@ class Main extends luxe.Game {
     // Scale camera's viewport accordingly when game is scaled, common and suitable for most games
 	// override function onwindowsized(e: luxe.Screen.WindowEvent) {
     //     Luxe.camera.viewport = new luxe.Rectangle(0, 0, e.event.x, e.event.y);
-    // }
-
-    // override function onrender() {
-    //     Luxe.draw.rectangle({
-    //         x: 20,
-    //         y: 20,
-    //         w: Luxe.screen.w - 40,
-    //         h: Luxe.screen.h - 40,
-    //         immediate: true
-    //     });
     // }
 
     override function onprerender() {
